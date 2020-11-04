602ea84615ca02f4c7517e3b2cb471953436502b
Florian Rodriguez <florian@negative-network.com>
2020-11-04 18:33:24 +0100

    init

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {TodoListData} from '../dataTypes/TodoListData';
+import {TodoItemData} from '../dataTypes/TodoItemData';
+import {TodoService} from '../todo.service';
+
+@Component({
+    selector: 'app-todo-list',
+    templateUrl: './todo-list.component.html',
+    styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+    private todoList: TodoListData; 
+    
+    constructor(private todoService: TodoService) {
+        todoService.getTodoListDataObservable().subscribe( tdl => this.todoList = tdl );
+    }
+
+    ngOnInit() {
+    }
+    
+    get label(): string {
+        return this.todoList.label;
+    }
+    
+    get items(): TodoItemData[] {
+        return this.todoList.items;
+    }
+}
