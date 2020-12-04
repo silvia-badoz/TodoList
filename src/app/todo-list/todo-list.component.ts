324b0ce0222efa0cef6867d98d5cde996dbb1517
Silvia Badoz-griffond <silviabadoz-griffond@MacBook-Air-de-Silvia.local>
2020-12-04 19:27:31 +0100

    LocalStorage et Effacer tout

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,6 +4,8 @@ import {TodoItemData} from '../dataTypes/TodoItemData';
 import {TodoService} from '../todo.service';
 import { stringify } from 'querystring';
 import { NgIf } from '@angular/common';
+import {Observable, BehaviorSubject} from 'rxjs';
+
 
 @Component({
     selector: 'app-todo-list',
@@ -14,7 +16,7 @@ export class TodoListComponent implements OnInit {
 
     private todoList: TodoListData; 
     stateBeforeEdit: string;
-    filter: string; 
+    filter: string;  
  
     constructor(private todoService: TodoService) {
         todoService.getTodoListDataObservable().subscribe( tdl => this.todoList = tdl );
@@ -22,10 +24,26 @@ export class TodoListComponent implements OnInit {
 
     ngOnInit() {
         this.filter = 'all'; 
+
+    /*    for (let i=0; i<localStorage.length; i++) { //permet d'afficher la liste depuis local storage quand la page est rafraîchie 
+            let index = localStorage.key(i);  //let index
+            let item = JSON.parse(localStorage.getItem(index)); //let item
+            this.appendItem(item.label); 
+            //console.log(item, index);
+            console.log(localStorage); 
+           // this.appendItem();
+         //   let localStorageItem = JSON.parse(localStorage.getItem('todoList'));
+          //  return localStorageItem == null ? [] : localStorageItem.todoList;
+           // console.log("item.label : " + item.label); 
+           // console.log("index : " + index); //todoList (clé)
+        } */
+
+        //this.refresh(this.label); 
+        this.refresh(this.label); 
     }
     
     get label(): string {
-        return this.todoList.label;
+        return this.todoList.label; 
     }
     
     get items(): TodoItemData[] {
@@ -37,36 +55,94 @@ export class TodoListComponent implements OnInit {
         if (label == '' || label == wronglabel) { //Condition qui n'ajoute pas une tâche vide ou une tâche ne comportant qu'un espace
             return; 
         } 
-        this.todoService.appendItems({
+
+        let item = { //mettre dans une variable tout l'objet item et ses attributs 
+            label, 
+            isDone:false,
+            editing:false
+        }
+        this.todoService.appendItems(item); 
+      /*  this.todoService.appendItems({
             label,
             isDone:false,
             editing:false
-        }) 
+        })  */
+
+        localStorage.setItem(item.label, JSON.stringify(item)); // créer un objet dans local storage en même temps que la création d'une tâche dans la liste
+        console.log(localStorage);
+    }
+
+    add(item:TodoItemData){
+        let wronglabel = ' ';
+        if (item.label == '' || item.label == wronglabel) { //Condition qui n'ajoute pas une tâche vide ou une tâche ne comportant qu'un espace
+            return; 
+        } 
+        this.todoService.appendItems(item);
+        localStorage.setItem(item.label, JSON.stringify(item));
+    }
+
+    refresh(label: string) {
+        for (let i=0; i<localStorage.length; i++) { //permet d'afficher la liste depuis local storage quand la page est rafraîchie 
+            let index = localStorage.key(i);  
+            let item = JSON.parse(localStorage.getItem(index)); 
+            //this.add(item.label); 
+            this.add(item);
+            console.log("local storage : " + localStorage); 
+    }
+
     }
 
     itemDone(item: TodoItemData, done:boolean) {
-        this.todoService.setItemsDone(done, item); 
-    }
+        let test = true; 
+      
+          if (item.isDone == false) {
+            this.todoService.setItemsDone(done, item); 
+            //local storage 
+            item.isDone = true; 
+            localStorage.setItem(item.label, JSON.stringify(item)); //mise à jour de l'élément dans local storage (pour isdone)
+            //
+            test = false;
+            }
+        
+        if(test) {
+            this.todoService.setItemsDone(done, item); 
+            //local storage 
+            item.isDone = false; 
+            localStorage.setItem(item.label, JSON.stringify(item)); //mise à jour de l'élément dans local storage (pour isdone)
+            //
+            test = false; 
+        }
+      }
 
-    // ----- MODIFS -----
     editTodo(item: TodoItemData): void {
         this.stateBeforeEdit = item.label; 
         item.editing = true; 
+        //local storage FONCTIONNE PAS 
+        localStorage.setItem(item.label, JSON.stringify(item)); //met editing à true dans local storage 
     }
 
     doneEdit(item: TodoItemData): void { 
         if (item.label.length === 0) {
            this.itemDelete(item); //permet de supprimer une tâche dans la liste (si en modifiant une tâche sa valeur vaut '')
+           //local storage FONCTIONNE PAS 
+           localStorage.removeItem(item.label); //supprime l'objet dans local storage 
+           // localStorage.setItem(item.label, JSON.stringify(item)); 
         } 
-        item.editing = false; 
+      //  localStorage.removeItem(item.label); //supp l'ancien item 
+      
+       item.editing = false; 
+       localStorage.setItem(item.label, JSON.stringify(item)); //met à false editing dans local storage quand fin de l'edit
+
     }
 
     cancelEdit(item: TodoItemData): void {
         item.label = this.stateBeforeEdit; 
         item.editing = false; 
+        //local storage 
+        localStorage.setItem(item.label, JSON.stringify(item)); //mettre editing à false si touche esc appuyée pour annuler l'edit
     }
 
-    itemsLeft() {
+    itemsLeft() { //compter le nombre de tâches restantes 
         let itemsLeft = 0; 
         for (var item of this.todoList.items) {
             if (item.isDone == false) {
@@ -94,6 +170,8 @@ export class TodoListComponent implements OnInit {
         for (var item of this.todoList.items) {
             if (item.isDone == true) {
                 this.todoService.removeItems(item); 
+                //local storage 
+                localStorage.removeItem(item.label); //supprime l'objet dans local storage 
             }
         }
     }
@@ -103,12 +181,20 @@ export class TodoListComponent implements OnInit {
         for (var item of this.todoList.items) {
             if (item.isDone == false) {
                 this.todoService.setItemsDone(true,item); 
+                //local storage 
+                item.isDone = true; 
+                localStorage.setItem(item.label, JSON.stringify(item)); //mise à jour de l'élément dans local storage (pour isdone)
+                //
                 test = false;
             }
         }
             if(test) {
                 for (var item of this.todoList.items) {
                     this.todoService.setItemsDone(false,item); 
+                    //local storage 
+                    item.isDone = false; 
+                    localStorage.setItem(item.label, JSON.stringify(item)); //mise à jour de l'élément dans local storage (pour isdone)
+                    //
                 }
                 test = false; 
             }
@@ -127,17 +213,34 @@ export class TodoListComponent implements OnInit {
         else if (this.filter === 'completed') {
             return this.todoList.items.filter(item => item.isDone);
             }
-        } //fin filterState() 
+        } 
     
-    
-    // ----- FIN MODIFS ----- 
-
-    itemLabel(item: TodoItemData, label:string) {
-        this.todoService.setItemsLabel(label, item); 
+    itemLabel(item: TodoItemData, label:string) { //MARCHE PAS !! 
+       // this.todoService.setItemsLabel(label, item); 
+        //local storage
+        // pour modifier l'item dans le localStorage, je le supprime dans un premier temps du localStorage, puis je remet le nouvel item modifié 
+        // Changer le nom de l'item
+        localStorage.removeItem(item.label); // Supprimer l'item du localStorage 
+        this.todoService.setItemsLabel(label,item);
+        item.label = label; 
+        localStorage.setItem(item.label, JSON.stringify(item)); // Remettre l'élément dans le localStorage
+  
     }
 
-    itemDelete(item:TodoItemData){
+    itemDelete(item:TodoItemData){ //bouton "croix" à droite de chaque item 
         this.todoService.removeItems(item);
+        //localStorage
+        localStorage.removeItem(item.label); //supprime l'objet dans local storage 
     }
+
+    deleteAll(item:TodoItemData){ //bouton "Tout supprimer" du footer 
+        for (var item of this.todoList.items) { 
+            this.todoService.removeItems(item); //supprime les items un à un 
+        }
+        localStorage.clear(); //supprime tous les items du local storage 
+    }
+
+    //speech recognition
+    
 
 }
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import {TodoListData} from './dataTypes/TodoListData';
 import {Observable, BehaviorSubject} from 'rxjs';
 import {TodoItemData} from './dataTypes/TodoItemData';
+import { isNgTemplate } from '@angular/compiler';
 
 @Injectable()
 export class TodoService {
 
   private todoListSubject = new BehaviorSubject<TodoListData>( {label: 'TodoList', items: []} );
+ 
 
-  constructor() { }
+  constructor() { 
+    //local storage 
+   // this.todoListSubject = new BehaviorSubject<TodoListData>(JSON.parse(localStorage.getItem("todoList")));
+
+  }
 
   getTodoListDataObservable(): Observable<TodoListData> {
     return this.todoListSubject.asObservable();
@@ -20,6 +26,8 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label, isDone: I.isDone, editing: I.editing}) )
     });
+    //local storage
+   // this.updateLS();
   }
 
   setItemsDone(isDone: boolean, ...items: TodoItemData[] ) {
@@ -28,6 +36,7 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label: I.label, isDone, editing: I.editing}) )
     });
+  //  this.updateLS();
   }
 
   appendItems( ...items: TodoItemData[] ) {
@@ -36,6 +45,7 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: [...tdl.items, ...items]
     });
+  //  this.updateLS();
   }
 
   removeItems( ...items: TodoItemData[] ) {
@@ -44,16 +54,23 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: tdl.items.filter( I => items.indexOf(I) === -1 )
     });
+  //  this.updateLS();
   }
 
-  // MODIFS : COMPTER NOMBRE TACHES RESTANTES /////////////////////
-  countLeft(isDone: false, ...items: TodoItemData[] ) {
-    const tdl = this.todoListSubject.getValue();
-    this.todoListSubject.next( {
-      label: tdl.label, 
-      items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label: I.label, isDone, editing: I.editing}) )  
-    });
+ /*
+  //remplissage dans local storage
+   addLS(){
+    if(!localStorage.getItem('todoList')){
+      let item = JSON.stringify(this.todoListSubject.getValue());
+      localStorage.setItem("todoList", item);
+    }
   }
-  // ////////////////////////////////
+
+  //mise à jour dans local storage
+  updateLS(){
+    var newItem = JSON.stringify(this.todoListSubject.getValue());
+    localStorage.setItem("todoList", newItem);
+  } 
+  */
 
 }
