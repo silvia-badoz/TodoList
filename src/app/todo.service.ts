9aa6543827c090b83db81a8357529e48c63665ed
Silvia Badoz <63328302+silvia-badoz@users.noreply.github.com>
2020-12-19 17:54:32 +0100

    comments

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -11,9 +11,6 @@ export class TodoService {
  
 
   constructor() { 
-    //local storage 
-   // this.todoListSubject = new BehaviorSubject<TodoListData>(JSON.parse(localStorage.getItem("todoList")));
-
   }
 
   getTodoListDataObservable(): Observable<TodoListData> {
@@ -26,8 +23,7 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label, isDone: I.isDone, editing: I.editing}) )
     });
-    //local storage
-   // this.updateLS();
+
   }
 
   setItemsDone(isDone: boolean, ...items: TodoItemData[] ) {
@@ -36,7 +32,7 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label: I.label, isDone, editing: I.editing}) )
     });
-  //  this.updateLS();
+
   }
 
   appendItems( ...items: TodoItemData[] ) {
@@ -45,7 +41,7 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: [...tdl.items, ...items]
     });
-  //  this.updateLS();
+
   }
 
   removeItems( ...items: TodoItemData[] ) {
@@ -54,23 +50,8 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: tdl.items.filter( I => items.indexOf(I) === -1 )
     });
-  //  this.updateLS();
+
   }
 
- /*
-  //remplissage dans local storage
-   addLS(){
-    if(!localStorage.getItem('todoList')){
-      let item = JSON.stringify(this.todoListSubject.getValue());
-      localStorage.setItem("todoList", item);
-    }
-  }
-
-  //mise à jour dans local storage
-  updateLS(){
-    var newItem = JSON.stringify(this.todoListSubject.getValue());
-    localStorage.setItem("todoList", newItem);
-  } 
-  */
 
 }
